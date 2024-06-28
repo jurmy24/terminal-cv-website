a10492c0409f898ca63a3b3c311c5f6dd8440c74
Victor Oldensand <victormag24@gmail.com>
2024-06-28 19:57:27 +0200

    Build: for vscode stuff

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,16 +2,16 @@
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
-  root: true,
-  extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  },
-  rules: {
-    'vue/multi-word-component-names': 0
-  }
+    root: true,
+    extends: [
+        'plugin:vue/vue3-essential',
+        'eslint:recommended',
+        '@vue/eslint-config-prettier/skip-formatting'
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest'
+    },
+    rules: {
+        'vue/multi-word-component-names': 0
+    }
 }
--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,15 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    server: {
+        watch: {
+            usePolling: true
+        }
     }
-  },
-  server: {
-    watch: {
-      usePolling: true
-    }
-  }
 })
