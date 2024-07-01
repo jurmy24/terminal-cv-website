15bbe6be8d4c694050a0334f3e5820900b1aee75
Victor Oldensand <victormag24@gmail.com>
2024-07-01 11:09:34 +0200

    Refactor: move stuff around and clean codebase for readability

---
+++ src/components/SideBarIcon.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="sidebar-icon group">
+        <slot></slot>
+        <span class="sidebar-tooltip group-hover:scale-100">{{ text }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SideBarIcon',
+    props: {
+        text: {
+            type: String,
+            default: 'tooltip'
+        }
+    }
+}
+</script>
