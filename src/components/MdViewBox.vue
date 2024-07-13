a2ddef3f7bbc84118f2c7b922acc022ae41e1fb2
Victor Oldensand <victormag24@gmail.com>
2024-07-13 10:36:46 +0200

    Fix: all links open new tabs now

--- src/components/MdViewBox.vue
+++ src/components/MdViewBox.vue
@@ -29,6 +29,22 @@ const markdowner = new MarkdownIt({
     linkify: true
 })
 
+// TO MAKE ALL LINKS OPEN IN NEW TAB
+// Remember the old renderer if overridden, or proxy to the default renderer.
+var defaultRender =
+    markdowner.renderer.rules.link_open ||
+    function (tokens, idx, options, env, self) {
+        return self.renderToken(tokens, idx, options)
+    }
+
+markdowner.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+    // Add a new `target` attribute, or replace the value of the existing one.
+    tokens[idx].attrSet('target', '_blank')
+
+    // Pass the token to the default renderer.
+    return defaultRender(tokens, idx, options, env, self)
+}
+
 export default {
     name: 'MdViewBox',
     props: {
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,5 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/terminal-cv-website/',
     plugins: [vue()]
 })
