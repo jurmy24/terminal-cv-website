fe42ef33e5e313efe81842714d8e6fd261d962cc
Victor Oldensand <victormag24@gmail.com>
2024-07-13 12:04:11 +0200

    Misc: vite config change again

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/terminal-cv-website/',
+    // base: '/terminal-cv-website/',
     plugins: [vue()],
     resolve: {
         alias: []
